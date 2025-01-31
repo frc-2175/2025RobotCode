4714190ccd986d01d7e974b9ad392cace2a2746b
2175 L16 <info@fightingcalculators.org>
2025-01-30 19:29:44 -0600

    Added swerve kinematics.

--- robot.py
+++ robot.py
@@ -1,16 +1,36 @@
 # TODO: insert robot code here
-
+from wpimath.geometry import Translation2d
 import wpilib
 import rev
+import wpimath
+from wpimath.kinematics import SwerveDrive4Kinematics, ChassisSpeeds, SwerveModuleState
+import ntcore
+import wpimath.units
 
-testMotorID = 21
+wheelDistanceFromCenter = wpimath.units.inchesToMeters(12.375)
+frontLeftLocation = Translation2d(wheelDistanceFromCenter, wheelDistanceFromCenter)
+frontRightLocation = Translation2d(wheelDistanceFromCenter, -wheelDistanceFromCenter)
+backLeftLocation = Translation2d(-wheelDistanceFromCenter, wheelDistanceFromCenter)
+backRightLocation = Translation2d(-wheelDistanceFromCenter, -wheelDistanceFromCenter)
+
+kinematics = SwerveDrive4Kinematics(frontLeftLocation,frontRightLocation,backLeftLocation,backRightLocation)
+nt= ntcore.NetworkTableInstance.getDefault()
+topic=nt.getStructArrayTopic("/SwerveStates", SwerveModuleState)
+pub= topic.publish()
 
 class MyRobot(wpilib.TimedRobot):
+    leftStick = wpilib.Joystick(0)
+    rightStick = wpilib.Joystick(1)
+    gamePad = wpilib.XboxController(2)
+    
     def robotInit(self) -> None:
-        self.gamePad = wpilib.XboxController(0)
-
-        self.testMotor = rev.CANSparkMax(testMotorID, rev.CANSparkLowLevel.MotorType.kBrushless)
-        self.testMotor.setIdleMode(rev.CANSparkMax.IdleMode.kCoast)
+        # self.testMotor = rev.CANSparkMax(testMotorID, rev.CANSparkLowLevel.MotorType.kBrushless)
+        # self.testMotor.setIdleMode(rev.CANSparkMax.IdleMode.kCoast)
+        pass
 
     def teleopPeriodic(self) -> None:
-        self.testMotor.set(self.gamePad.getLeftY)
+        speeds= ChassisSpeeds(-self.leftStick.getY(), -self.leftStick.getX(), -self.rightStick.getX())
+        frontLeft, frontRight,backLeft,backRight= kinematics.toSwerveModuleStates(speeds)
+        pub.set([frontLeft,frontRight,backLeft,backRight])
+        pass
+
