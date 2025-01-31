d7f38446be830f67236c7146efd559fc2b6eb25d
2175 L16 <info@fightingcalculators.org>
2025-01-30 20:54:28 -0600

    We added all the motors as well as their swerve states,
    Problems:
    - Steering Encoders are inverted
    -Drive n coder velocity is the wrong units

--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ from wpimath.geometry import Translation2d
 import wpilib
 import rev
 import wpimath
+import wpimath.geometry
 from wpimath.kinematics import SwerveDrive4Kinematics, ChassisSpeeds, SwerveModuleState
 import ntcore
 import wpimath.units
@@ -15,22 +16,64 @@ backRightLocation = Translation2d(-wheelDistanceFromCenter, -wheelDistanceFromCe
 
 kinematics = SwerveDrive4Kinematics(frontLeftLocation,frontRightLocation,backLeftLocation,backRightLocation)
 nt= ntcore.NetworkTableInstance.getDefault()
-topic=nt.getStructArrayTopic("/SwerveStates", SwerveModuleState)
-pub= topic.publish()
+desiredSwerveStatesTopic=nt.getStructArrayTopic("/DesiredSwerveStates", SwerveModuleState).publish()
+actualSwerveStatesTopic=nt.getStructArrayTopic("/ActualSwerveStates", SwerveModuleState).publish()
 
 class MyRobot(wpilib.TimedRobot):
     leftStick = wpilib.Joystick(0)
     rightStick = wpilib.Joystick(1)
     gamePad = wpilib.XboxController(2)
+
+    frontLeftDriveMotor = rev.SparkMax(25, rev.SparkLowLevel.MotorType.kBrushless)
+    frontLeftSteerMotor = rev.SparkMax(21, rev.SparkLowLevel.MotorType.kBrushless)
+    frontRightDriveMotor = rev.SparkMax(28, rev.SparkLowLevel.MotorType.kBrushless)
+    frontRightSteerMotor = rev.SparkMax(22, rev.SparkLowLevel.MotorType.kBrushless)
+    backLeftDriveMotor = rev.SparkMax(26, rev.SparkLowLevel.MotorType.kBrushless)
+    backLeftSteerMotor = rev.SparkMax(24, rev.SparkLowLevel.MotorType.kBrushless)
+    backRightDriveMotor = rev.SparkMax(27, rev.SparkLowLevel.MotorType.kBrushless)
+    backRightSteerMotor = rev.SparkMax(23, rev.SparkLowLevel.MotorType.kBrushless)
+
+    frontLeftDriveEncoder = frontLeftDriveMotor.getEncoder()
+    frontLeftSteerEncoder = frontLeftSteerMotor.getAbsoluteEncoder()
+    frontRightDriveEncoder = frontRightDriveMotor.getEncoder()
+    frontRightSteerEncoder = frontRightSteerMotor.getAbsoluteEncoder()
+    backLeftDriveEncoder = backLeftDriveMotor.getEncoder()
+    backLeftSteerEncoder = backLeftSteerMotor.getAbsoluteEncoder()
+    backRightDriveEncoder = backRightDriveMotor.getEncoder()
+    backRightSteerEncoder = backRightSteerMotor.getAbsoluteEncoder()
+
+    # TODO: Rev seems to have removed this method in 2025.
+    # frontLeftSteerEncoder.setInverted(True)
+    # frontRightSteerEncoder.setInverted(True)
+    # backLeftSteerEncoder.setInverted(True)
+    # backRightSteerEncoder.setInverted(True)
+
+    def __setattr__(self, name, value):
+        self.frontLeftSteerEncoder.getVelocity()
     
-    def robotInit(self) -> None:
-        # self.testMotor = rev.CANSparkMax(testMotorID, rev.CANSparkLowLevel.MotorType.kBrushless)
-        # self.testMotor.setIdleMode(rev.CANSparkMax.IdleMode.kCoast)
-        pass
+    def robotPeriodic(self):
+        actualFrontLeft = SwerveModuleState(
+            self.frontLeftDriveEncoder.getVelocity(),
+            wpimath.geometry.Rotation2d(self.frontLeftSteerEncoder.getPosition())
+        )
+        actualFrontRight = SwerveModuleState(
+            self.frontRightDriveEncoder.getVelocity(),
+            wpimath.geometry.Rotation2d(self.frontRightSteerEncoder.getPosition())
+        )
+        actualBackLeft = SwerveModuleState(
+            self.backLeftDriveEncoder.getVelocity(),
+            wpimath.geometry.Rotation2d(self.backLeftSteerEncoder.getPosition())
+        )
+        actualBackRight = SwerveModuleState(
+            self.backRightDriveEncoder.getVelocity(),
+            wpimath.geometry.Rotation2d(self.backRightSteerEncoder.getPosition())
+        )
+        actualSwerveStatesTopic.set([actualFrontLeft,actualFrontRight,actualBackLeft,actualBackRight])
+
 
     def teleopPeriodic(self) -> None:
         speeds= ChassisSpeeds(-self.leftStick.getY(), -self.leftStick.getX(), -self.rightStick.getX())
         frontLeft, frontRight,backLeft,backRight= kinematics.toSwerveModuleStates(speeds)
-        pub.set([frontLeft,frontRight,backLeft,backRight])
+        desiredSwerveStatesTopic.set([frontLeft,frontRight,backLeft,backRight])
         pass
 
