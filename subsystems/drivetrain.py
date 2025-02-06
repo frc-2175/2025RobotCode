78c2b652f4f9abaa55502f6be3d141e3ba4d7c78
2175 L11 <info@fightingcalculators.org>
2025-02-05 19:38:07 -0600

    Swerve Module Gyro

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -1,8 +1,9 @@
+import navx
 import rev
 import constants
 import math
 import wpimath
-from wpimath.geometry import Translation2d
+from wpimath.geometry import Translation2d, Rotation2d
 from wpimath.kinematics import SwerveDrive4Kinematics, ChassisSpeeds, SwerveModuleState
 import ntcore
 from swervemodule import SwerveModule
@@ -18,11 +19,15 @@ nt= ntcore.NetworkTableInstance.getDefault()
 desiredSwerveStatesTopic=nt.getStructArrayTopic("/DesiredSwerveStates", SwerveModuleState).publish()
 actualSwerveStatesTopic=nt.getStructArrayTopic("/ActualSwerveStates", SwerveModuleState).publish()
 
+gyroTopic = nt.getStructTopic("/Gyro", Rotation2d).publish()
+
 class Drivetrain:
     frontLeftSwerveModule = SwerveModule(25, 21, 3*math.pi/2)
     frontRightSwerveModule = SwerveModule(28, 22, 0)
     backLeftSwerveModule = SwerveModule(26, 24, math.pi)
     backRightSwerveModule = SwerveModule(27, 23, math.pi/2)
+
+    gyro = navx.AHRS.create_spi()
 
     def periodic(self):
         actualFrontLeft = self.frontLeftSwerveModule.getState()
@@ -31,8 +36,10 @@ class Drivetrain:
         actualBackRight = self.backRightSwerveModule.getState()
         actualSwerveStatesTopic.set([actualFrontLeft,actualFrontRight,actualBackLeft,actualBackRight])
 
+        gyroTopic.set(self.gyro.getRotation2d())
+
     def drive(self, xSpeed: float, ySpeed: float, turnSpeed: float):
-        speeds= ChassisSpeeds(xSpeed, ySpeed, turnSpeed)
+        speeds= ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turnSpeed, self.gyro.getRotation2d())
         frontLeft, frontRight,backLeft,backRight= kinematics.toSwerveModuleStates(speeds)
         desiredSwerveStatesTopic.set([frontLeft,frontRight,backLeft,backRight])
 
