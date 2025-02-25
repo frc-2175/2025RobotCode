a640184d454f189f0629143500abe75d40650087
2175 L14 <info@fightingcalculators.org>
2025-02-24 20:42:54 -0600

    Attempted to smooth out drivetrain (WARNING: BUGGY)

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -5,6 +5,7 @@ import math
 import wpimath
 from wpimath.geometry import Translation2d, Rotation2d
 from wpimath.kinematics import SwerveDrive4Kinematics, ChassisSpeeds, SwerveModuleState
+from wpimath.filter import SlewRateLimiter
 import ntcore
 from swervemodule import SwerveModule
 
@@ -18,6 +19,8 @@ kinematics = SwerveDrive4Kinematics(frontLeftLocation,frontRightLocation,backLef
 nt= ntcore.NetworkTableInstance.getDefault()
 desiredSwerveStatesTopic=nt.getStructArrayTopic("/DesiredSwerveStates", SwerveModuleState).publish()
 actualSwerveStatesTopic=nt.getStructArrayTopic("/ActualSwerveStates", SwerveModuleState).publish()
+desiredChassisSpeedsTopic=nt.getStructTopic("/DesiredChassisSpeeds", ChassisSpeeds).publish()
+currentChassisSpeedsTopic=nt.getStructTopic("/CurrentChassisSpeeds", ChassisSpeeds).publish()
 
 gyroTopic = nt.getStructTopic("/Gyro", Rotation2d).publish()
 
@@ -29,7 +32,36 @@ class Drivetrain:
 
     gyro = navx.AHRS.create_spi()
 
+    speedLimiter = SlewRateLimiter(1) #m/s
+    rotationLimiter = SlewRateLimiter(math.pi) #rad/s
+    directionLimiter = SlewRateLimiter(math.pi) #rad/s
+
+    desiredChassisSpeeds = ChassisSpeeds(0, 0, 0)
+    currentChassisSpeeds = ChassisSpeeds(0, 0, 0)
+    
     def periodic(self):
+        desiredSpeed = math.sqrt(self.desiredChassisSpeeds.vx**2 + self.desiredChassisSpeeds.vy**2)
+        newSpeed = self.speedLimiter.calculate(desiredSpeed)
+
+        desiredDirection = math.atan2(self.desiredChassisSpeeds.vy, self.desiredChassisSpeeds.vx)
+        newDirection = self.directionLimiter.calculate(desiredDirection)
+        newTurnSpeed = self.rotationLimiter.calculate(self.desiredChassisSpeeds.omega)
+
+        newVX = newSpeed*math.cos(newDirection)
+        newVY = newSpeed*math.sin(newDirection)
+        self.currentChassisSpeeds = ChassisSpeeds(newVX, newVY, newTurnSpeed)
+
+        frontLeft, frontRight,backLeft,backRight= kinematics.toSwerveModuleStates(self.currentChassisSpeeds)
+        desiredSwerveStatesTopic.set([frontLeft,frontRight,backLeft,backRight])
+
+        desiredChassisSpeedsTopic.set(self.desiredChassisSpeeds)
+        currentChassisSpeedsTopic.set(self.currentChassisSpeeds)
+
+        self.frontLeftSwerveModule.setState(frontLeft)
+        self.frontRightSwerveModule.setState(frontRight)
+        self.backLeftSwerveModule.setState(backLeft)
+        self.backRightSwerveModule.setState(backRight)
+
         actualFrontLeft = self.frontLeftSwerveModule.getState()
         actualFrontRight = self.frontRightSwerveModule.getState()
         actualBackLeft = self.backLeftSwerveModule.getState()
@@ -37,16 +69,10 @@ class Drivetrain:
         actualSwerveStatesTopic.set([actualFrontLeft,actualFrontRight,actualBackLeft,actualBackRight])
 
         gyroTopic.set(self.gyro.getRotation2d())
+        
 
     def drive(self, xSpeed: float, ySpeed: float, turnSpeed: float):
-        speeds= ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turnSpeed, self.gyro.getRotation2d())
-        frontLeft, frontRight,backLeft,backRight= kinematics.toSwerveModuleStates(speeds)
-        desiredSwerveStatesTopic.set([frontLeft,frontRight,backLeft,backRight])
-
-        self.frontLeftSwerveModule.setState(frontLeft)
-        self.frontRightSwerveModule.setState(frontRight)
-        self.backLeftSwerveModule.setState(backLeft)
-        self.backRightSwerveModule.setState(backRight)
+        self.desiredChassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turnSpeed, self.gyro.getRotation2d())
 
     def reset_heading(self):
         self.gyro.reset()
